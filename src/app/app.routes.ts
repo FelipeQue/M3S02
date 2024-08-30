e52feda29f23375714efaba362982af35a57eea1
Felipe Quérette <felipe.querette@gmail.com>
2024-08-30 17:50:14 -0300

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
+import { VehicleDetailComponent } from './components/vehicle-detail/vehicle-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/vehiclelist",
+        pathMatch: "full"
+    },
+    {
+        path: "vehiclelist",
+        children: [
+            { path: "",component: VehicleListComponent },
+            { path: ":id", component: VehicleDetailComponent },
+        ],
+    },
+];

ee6d68d7fc16b9e00c6c2a096ce619dc2dcf74b7
Felipe Quérette <felipe.querette@gmail.com>
2024-09-01 21:54:14 -0300

    feature: add to/remove from favorites

--- src/app/components/vehicle-detail/vehicle-detail.component.ts
+++ src/app/components/vehicle-detail/vehicle-detail.component.ts
@@ -20,25 +20,59 @@ export class VehicleDetailComponent {
   datePipe = inject(DatePipe);
   faHeart = faHeart;
   vehicle: any = {};
+  isFavorite: boolean = false;
 
-  ngOnInit(){
+  ngOnInit(): void {
     this.activatedRoute.params.subscribe({
       next: (params: any)=>{
         this.vehicleService.getById(params.id).subscribe({
           next: (data)=>{
             this.vehicle = data;
+            this.checkIsFavorite();
           },
           error:(error)=>{ console.log("Erro ao buscar veículos por id",error) }
         })
       },
       error:(error)=>{ console.log("Erro ao capturar os parâmetros de rota",error) },
     })
-
   }
 
+  checkIsFavorite(): void {
+    let storage = localStorage.getItem('favoritos'); 
+    if(storage){ 
+      let favorites = JSON.parse(storage);
+      this.isFavorite = favorites.some((favorite: any)=>favorite.id == this.vehicle.id);
+    } else{
+      localStorage.setItem('favoritos', "[]");
+      this.isFavorite = false;
+    }
+  }
 
-  getYear(dateString: string): any {
+  getYear(dateString: string): string | null {
     return this.datePipe.transform(dateString, 'yyyy');
   }
 
-}
+  toggleFavorites(): void{
+    let storage = localStorage.getItem('favoritos'); 
+    if(storage){ 
+      let favorites = JSON.parse(storage);
+      if (this.isFavorite){
+        favorites = favorites.filter((favorite: any) => favorite.id !== this.vehicle.id);
+        this.isFavorite = false;
+        alert("Veículo removido da lista de favoritos.");
+      } else {
+        favorites.push(this.vehicle);
+        this.isFavorite = true;
+        alert("Veículo adicionado à lista de favoritos!");
+      }
+      localStorage.setItem('favoritos', JSON.stringify(favorites));
+    }
+  }
+
+
+  }
+
+
+
+
+
